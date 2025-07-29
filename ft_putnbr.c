4f8dda18451a430dea6cca2a6fb659cfc4ee2d29
rainbowcod <rainbowmelone01@gmail.com>
2025-07-29 14:09:52 +0200

    added pustr and wrote smth in printf

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,34 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 18:10:44 by olmatske          #+#    #+#             */
-/*   Updated: 2025/07/25 18:10:45 by olmatske         ###   ########.fr       */
+/*   Updated: 2025/07/28 17:39:30 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	putnbr(int n)
+{
+	long	num;
+
+	num = n;
+	if (num < 0)
+	{
+		ft_putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		putnbr(num / 10);
+		putnbr(num % 10);
+	}
+	else
+		ft_putchar(num + '0');
+	return (num);
+}
+int	ft_putchar(char	c)
+{
+	if ((write(1, &c, 1)) == -1)
+		return (-1);
+	return (1);
+}
--- ft_putstr.c
+++ ft_putstr.c
@@ -1,29 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/25 17:51:52 by olmatske          #+#    #+#             */
-/*   Updated: 2025/07/28 18:10:10 by olmatske         ###   ########.fr       */
+/*   Created: 2025/07/28 17:20:22 by olmatske          #+#    #+#             */
+/*   Updated: 2025/07/28 17:40:00 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+#include "libft.h"
 
-static int	format_type(char c);
-static int	hexconvert(int n);
-
-int	ft_printf(const char *format, ...)
+int	ft_putstr(char *str)
 {
-	va_list	args;
-	va_start(args, count);
-	
-}
+	int	i;
 
-static int	format_type(char c)
-{
-	if (c == "c")
-		return ()
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == NULL)
+			return (ft_putstr("(null)"));
+		ft_putchar(str[i]);
+		i++;
+	}
+	return (i);
 }
